68354bee8c1113cfbdf2cebd5c176961d8edad86
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2023-09-12 15:56:11 +0200

    stap 3

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,25 @@
 from flask import Flask
 from flask_cors import CORS
 
+import os
+import openai
+
+openai.api_key = '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'
+
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{
+            "role": "system",
+            "content": "wat is de hoofdstad van italie"
+        }],
+        temperature=0.5,
+        max_tokens=256
+    )
+    #print(response)
+    return response["choices"][0]["message"]["content"]
